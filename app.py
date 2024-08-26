9daf3b73c4fdee5b68c163cdcb09b34c44af92a0
Davi Ximenes <82062555+davixmns@users.noreply.github.com>
2024-08-26 17:59:16 -0300

    plotando grafico

--- app.py
+++ app.py
@@ -1,33 +1,53 @@
+import matplotlib.pyplot as plt
 from datetime import datetime
 import random
+import time
 from search.binary_search import binary_search
 from sort.merge_sort import merge_sort
 from sort.quick_sort import quick_sort
 
-def execute_sort(func, arr):
-    start = datetime.now()
-    result = func(arr)
-    end = datetime.now()
-    execution_time = (end - start).total_seconds() * 1000
-    print(f"{func.__name__}: {execution_time:.3f}ms")
-    return result
+def execute_sort_algorithms(algorithms, iterations, array_size=10000):
+    results = {}
+    
+    for sort_func in algorithms:
+        results[sort_func.__name__] = []
+    
+    for i in range(iterations):
+        random_numbers = [random.randint(1, array_size) for _ in range(array_size)]
+        
+        for sort_func in algorithms:
+            start_time = time.time()
+            sort_func(random_numbers.copy())
+            end_time = time.time()
+            execution_time = (end_time - start_time) * 1000 # Milliseconds
+            execution_time = round(execution_time, 3)
+            results[sort_func.__name__].append(execution_time)
+            
+    return results
 
-def execute_search(func, arr, target):
-    start = datetime.now()
-    result = func(arr, target)
-    end = datetime.now()
-    execution_time = (end - start).total_seconds() * 1000
-    print(f"{func.__name__}: {execution_time:.3f}ms")
-    return result
+def plot_results(results, iterations, array_size):
+    avg_exec_times = {algo: sum(exec_times) / len(exec_times) for algo, exec_times in results.items()}
 
+    print(avg_exec_times)
 
-max = 1000
-sorted_numbers = [i for i in range(1, max)]
-random_numbers = [random.randint(1, max) for i in range(1, max)]
-random_number = random.randint(1, max)
+    # Create bar plot
+    plt.figure(figsize=(8, 6))
+    plt.bar(avg_exec_times.keys(), avg_exec_times.values(), color=['blue', 'orange'])
 
-execute_sort(merge_sort, random_numbers)
+    # Add labels and title
+    plt.ylabel('Execution Time (ms)')
+    plt.title(f'Comparison of Sorting Algorithms\n{iterations} Iteration(s) - Array Size: {array_size}')
 
-execute_sort(quick_sort, random_numbers)
+    # Add exact values on top of the bars
+    for i, v in enumerate(avg_exec_times.values()):
+        plt.text(i, v, str(round(v, 7)), ha='center', va='bottom')
 
-execute_search(binary_search, sorted_numbers, random_number)
+    plt.grid(axis='y', linestyle='--', alpha=0.7)
+    
+    # plt.show()
+            
+algorithms_to_compare = [merge_sort, quick_sort]
+
+exec_times_result = execute_sort_algorithms(algorithms_to_compare, iterations=1, array_size=1000)
+
+plot_results(exec_times_result, iterations=1, array_size=1000)
