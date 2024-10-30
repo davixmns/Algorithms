35be36a589c1413a4dc836acb65c8a6d10dd52f7
Davi Ximenes <82062555+davixmns@users.noreply.github.com>
2024-10-30 09:32:46 -0300

    binary search com while

--- search/binary_search.py
+++ search/binary_search.py
@@ -1,17 +1,36 @@
 def binary_search(array, target):
-    return binary_search_recursive(array, target, 0, len(array) - 1)
+    left = 0
+    right = len(array) - 1
+
+    while left <= right:
+        mid = (right + left) // 2
+
+        if array[mid] == target:
+            return mid
+
+        if target < array[mid]:
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    return -1
+
 
 def binary_search_recursive(array, target, left, right):
-        if(left > right):
-            return -1
-        
-        mid = left + (right - left) // 2
+    if left > right:
+        return -1
 
-        if(array[mid] == target):
-            return mid
-        
-        if(target < array[mid]):
-            return binary_search_recursive(array, target, left, mid - 1)
-        else:
-            return binary_search_recursive(array, target, mid + 1, right)
-        
+    mid = (right + left) // 2
+
+    if array[mid] == target:
+        return mid
+
+    if target < array[mid]:
+        return binary_search_recursive(array, target, left, mid - 1)
+    else:
+        return binary_search_recursive(array, target, mid + 1, right)
+
+
+input = ["ana", "bob", "carl", "dave", "ed", "frank", "gus", "hank", "ian", "joeeeee"]
+
+print(binary_search_recursive(input, "ian", 0, len(input) - 1))
